1951b001e9e8e2af6f6fc94416e24c8b64dfd134
BoluwatifeOlusegun <boluwatifeolusegun018@gmail.com>
2024-05-08 02:56:58 -0300

    javascript fix

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var numberOfKeyButtons = document.querySelectorAll(".key").length;
 for(var i = 0; i < numberOfKeyButtons; i++){
 
     document.querySelectorAll(".key")[i].addEventListener("click",function(){
-        
         var audio = new Audio(/home/tifelabs/Desktop/animal - sounds/assets/sounds/cow.wav)
         audio.play()
     });
